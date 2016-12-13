f53432a0a5e7b0479bd97a30c5829ac361f0f8d8
shubidubapp <shubidubapp@gmail.com>
2016-12-13 13:44:52 +0300

    improved crawler

--- crawl_user.py
+++ crawl_user.py
@@ -7,7 +7,6 @@ import csv
 if __name__ == "__main__":
     model_list_source = get_model_list_source()
     model_advert_list = get_model_advert_list_urls(model_list_source)
-    all_urls = []
 
     with open("sample_output.csv", "w") as output_file:
         writer = csv.writer(output_file)
@@ -15,21 +14,28 @@ if __name__ == "__main__":
                      "Garanti", "İlan Tarihi", "Kilometre", "Yakıt Tipi", "Kapı Sayısı"]
         writer.writerow(label_row)
 
-    for url in tqdm(model_advert_list):
+    output_list = list()
+    for url in tqdm(model_advert_list[:20]):
         advert_urls = get_advert_urls(url)
-        all_urls.extend(advert_urls)
-        output_list = list()
-        for model_url in all_urls:
+        for model_url in advert_urls:
             page_source = get_page_source(model_url)
-            advert_details = get_advert_details(page_source)
-            output_list.append(advert_details)
-            with open("sample_output.csv", "w") as output_file:
+            advert = get_advert_details(page_source)
+            output = [advert["ilan_no"], advert["marka"], advert["model"], advert["seri"], advert["fiyat"],
+                      advert["model_yili"], advert["kasa_tipi"], advert["vites_tipi"], advert["renk"],
+                      advert["garanti"],
+                      advert["ilan_tarihi"], advert["km"], advert["yakit_tipi"], advert"kapi_sayisi"]
+            with open("sample_output.csv", "a") as output_file:
                 writer = csv.writer(output_file)
+                writer.writerow(output)
 
-                for advert in output_list:
-                    output = [advert["ilan_no"], advert["marka"], advert["model"], advert["seri"], advert["fiyat"],
-                              advert["model_yili"], advert["kasa_tipi"], advert["vites_tipi"], advert["renk"], advert["garanti"],
-                              advert["ilan_tarihi"], advert["km"], advert["yakit_tipi"], "kapısayısı"]
-
-                    writer.writerow(output)
+    # with open("sample_output.csv", "a") as output_file:
+    #     writer = csv.writer(output_file)
+    #
+    #     for advert in output_list:
+    #         output = [advert["ilan_no"], advert["marka"], advert["model"], advert["seri"], advert["fiyat"],
+    #                   advert["model_yili"], advert["kasa_tipi"], advert["vites_tipi"], advert["renk"],
+    #                   advert["garanti"],
+    #                   advert["ilan_tarihi"], advert["km"], advert["yakit_tipi"], advert"kapi_sayisi"]
+    #
+    #         writer.writerow(output)
 
